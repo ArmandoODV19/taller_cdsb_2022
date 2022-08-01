99414903ca9a1489635bef6b05ec8b7dd12675cd
armandios19 <fooseaov@gmail.com>
2022-08-01 09:47:49 -0500

    librerias de trabajo

---
+++ scripts/librerias_de_trabajo.R
@@ -0,0 +1,8 @@
+# Librerias de trabajo
+
+install.packages("rmarkdown")
+install.packages("usethis")
+install.packages("remotes")
+install.packages("bookdown")
+install.packages("xaringan")
+install.packages("devtools")
