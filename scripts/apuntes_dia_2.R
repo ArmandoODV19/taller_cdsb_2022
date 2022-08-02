277926f5cc58aea985a8eab7515fe1c7eb1b0065
Armando Ocampo <fooseaov@gmail.com>
2022-08-02 10:47:55 -0500

    trabajando con conda

--- scripts/apuntes_dia_2.R
+++ scripts/apuntes_dia_2.R
@@ -15,3 +15,12 @@ usethis::create_package("../../../../proyecto_prueba.Rproj")
 # funcion para evaluar el paquete en los tres sistemas operativos, version de R
 # y version de Bioconductor
 use_bioc_github_action()
+
+
+#### CONDA
+
+# para instalar paquetees en conda se utiliza la terminal. se utiliza el
+# siguiente comando
+
+# conda install PACKAGE
+
