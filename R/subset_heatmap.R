457c87d61d784e272f5c8e78bb9637c437ea448c
Armando Ocampo <fooseaov@gmail.com>
2022-08-04 12:39:38 -0500

    clase

---
+++ R/subset_heatmap.R
@@ -0,0 +1,18 @@
+#' Title
+#'
+#' @param x
+#' @param especies
+#' @param genes
+#'
+#' @return
+#' @export
+#'
+#' @examples
+subset_heatmap <- function(x, especies = NULL, genes = NULL) {
+  # subset matrix
+  x_subset <- x[genes,especies]
+  # plot heatmap
+  ComplexHeatmap::Heatmap(x_subset,
+                          cluster_columns = FALSE,
+                          heatmap_legend_param = list(title = "log2"))
+}
