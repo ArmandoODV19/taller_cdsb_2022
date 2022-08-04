d60943b7eb1badb749f52e8ba23493395075ba1f
Armando Ocampo <fooseaov@gmail.com>
2022-08-04 09:33:56 -0500

    ignorar rproj

--- scripts/creando_paquetes.R
+++ scripts/creando_paquetes.R
@@ -20,3 +20,8 @@ biocthis::use_bioc_pkg_templates() # esto corre en la consola, no se abre script
 
 # abre 4 scripts
 # en el primer scripst checar que tenemos las paqueterias que solicita
+
+
+# pedir que git ignore el archivo .Rproj
+
+usethis::use_git_ignore("*.Rproj") # esta linea se encuentra en el script 2
