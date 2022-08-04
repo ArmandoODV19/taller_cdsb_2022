95313c40bd9cb92f8ad056a5d3961c8645c634fd
Armando Ocampo <fooseaov@gmail.com>
2022-08-04 09:25:38 -0500

    opciones de consola

--- scripts/creando_paquetes.R
+++ scripts/creando_paquetes.R
@@ -16,7 +16,7 @@ usethis::create_package("pkgMetagenomica")
 
 # estructura de biocthis
 
-biocthis::use_bioc_pkg_templates()
+biocthis::use_bioc_pkg_templates() # esto corre en la consola, no se abre script
 
 # abre 4 scripts
 # en el primer scripst checar que tenemos las paqueterias que solicita
